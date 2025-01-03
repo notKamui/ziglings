57773998d1045774027a97ed9764480486537287
notKamui <teillard.jimmy@outlook.fr>
2025-01-03 15:32:42 +0100

    ex31

--- exercises/031_switch2.zig
+++ exercises/031_switch2.zig
@@ -26,11 +26,25 @@ pub fn main() void {
             8 => 'H',
             9 => 'I',
             10 => 'J',
-            // ...
+            11 => 'K',
+            12 => 'L',
+            13 => 'M',
+            14 => 'N',
+            15 => 'O',
+            16 => 'P',
+            17 => 'Q',
+            18 => 'R',
+            19 => 'S',
+            20 => 'T',
+            21 => 'U',
+            22 => 'V',
+            23 => 'W',
+            24 => 'X',
             25 => 'Y',
             26 => 'Z',
             // As in the last exercise, please add the 'else' clause
             // and this time, have it return an exclamation mark '!'.
+            else => '!',
         };
 
         std.debug.print("{c}", .{real_char});
